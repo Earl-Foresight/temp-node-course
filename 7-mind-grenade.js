685ac9fa718fdfad85b733bd68f34743adee34af
Earl <earl@fore-sight.tech>
2022-03-10 13:27:00 +0800

    first commit

---
+++ 7-mind-grenade.js
@@ -0,0 +1,10 @@
+const num1 = 5
+const num2 = 10
+
+function addValues() {
+    console.log(`num1 = ${num1}`);
+    console.log(`num2 = ${num2}`);
+    console.log(`the sum is: ${num1 + num2}`);
+}
+
+addValues()
